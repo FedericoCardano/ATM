f7b4eb95c92f434bfedd022676bdbda75fb2bd46
FedericoCardano <federico.cardano17@gmail.com>
2022-11-19 12:12:38 +0100

    Creazione EuroTest

---
+++ src/test/EuroTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class EuroTest {
+    @Test
+    void testGetValore() {
+
+    }
+
+    @Test
+    void testMinoreDi() {
+
+    }
+
+    @Test
+    void testSomma() {
+
+    }
+
+    @Test
+    void testSottrai() {
+
+    }
+
+    @Test
+    void testStampa() {
+
+    }
+
+    @Test
+    void testUgualeA() {
+
+    }
+}
